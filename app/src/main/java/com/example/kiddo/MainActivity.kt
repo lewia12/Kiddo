b071626a37785945fea51520fd93a70b4a84e27a
Raven <Lewia12@gmail.com>
2023-10-21 20:00:19 +0800

    New Alpha

--- app/src/main/java/com/example/kiddo/MainActivity.kt
+++ app/src/main/java/com/example/kiddo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.kiddo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
